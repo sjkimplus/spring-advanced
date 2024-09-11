92d8b6db5117f407478dc795305ab53d2a70aaf7
Sungju Kim <sjkim20010909@gmail.com>
2024-09-11 12:50:03 +0900

    레벨 1-4: JWT 유효성 검사 로직 수정

--- src/main/java/org/example/expert/domain/manager/controller/ManagerController.java
+++ src/main/java/org/example/expert/domain/manager/controller/ManagerController.java
@@ -38,12 +38,12 @@ public class ManagerController {
 
     @DeleteMapping("/todos/{todoId}/managers/{managerId}")
     public void deleteManager(
-            @RequestHeader("Authorization") String bearerToken,
+            @Auth AuthUser authUser,
             @PathVariable long todoId,
             @PathVariable long managerId
     ) {
-        Claims claims = jwtUtil.extractClaims(bearerToken.substring(7));
-        long userId = Long.parseLong(claims.getSubject());
-        managerService.deleteManager(userId, todoId, managerId);
+//        Claims claims = jwtUtil.extractClaims(bearerToken.substring(7));
+//        long userId = Long.parseLong(claims.getSubject());
+        managerService.deleteManager(authUser, todoId, managerId);
     }
 }
